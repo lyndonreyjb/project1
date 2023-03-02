9e25b91792cdefb9e889bc5d6af94908c4345984
lyndonrey <lyndonreybualat@gmail.com>
2023-03-01 23:50:50 -0700

    added navigation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import Home from "./components/routes/home/home.component";
-import { Routes, Route, Outlet } from "react-router-dom";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>Navigation Bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import Navigation from "./components/routes/navigation/navigation.component";
+import { Routes, Route } from "react-router-dom";
 
 const App = () => {
   return (
